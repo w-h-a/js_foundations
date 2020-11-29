95fb8026610068b4998331adc55d2d3dd07638c5
Wes Anderson <wesley.herb.anderson@gmail.com>
2020-11-29 17:57:23 -0500

    modified calculator.js, added .eslintrc.yml, and debugging examples

--- small_programs/calculator.js
+++ small_programs/calculator.js
@@ -1,39 +1,74 @@
 /*
-1. Ask the user for the first number.
-2. Ask the user for the second number.
-3. Ask the user for an operation to perform.
-4. Perform the operation on the two numbers.
-5. Print the result to the terminal.
+START
+
+1. GET the first number.
+2. Check if input is permissible, otherwise, ask for another
+3. GET the second number.
+4. Check if input is permissible, otherwise, ask for another
+5. GET the operator.
+6. Check if input is permissible, otherwise, ask again
+7. PRINT number operator number
+8. GET user's answer to the question "Would you like to do another?"
+
+END
 */
 
 const READLINE = require('readline-sync');
 
 function getUserInput(inputParam) {
-  return READLINE.question(inputParam);
+  return READLINE.question(`=> ${inputParam}`);
+}
+
+function impermissibleNumber(numberParam) {
+  return numberParam.trim() === '' || Number.isNaN(Number(numberParam));
 }
 
 function calculator(firstNumParam, secondNumParam, operationParam) {
   let output;
-  if (operationParam === '1') {
-    output = firstNumParam + secondNumParam;
-  } else if (operationParam === '2') {
-    output = firstNumParam - secondNumParam;
-  } else if (operation === '3') {
-    output = firstNumParam * secondNumParam;
-  } else if (operation === '4') {
-    output = firstNumParam / secondNumParam;
+  switch (operationParam) {
+    case '1':
+      output = firstNumParam + secondNumParam;
+      break;
+    case '2':
+      output = firstNumParam - secondNumParam;
+      break;
+    case '3':
+      output = firstNumParam * secondNumParam;
+      break;
+    case '4':
+      output = firstNumParam / secondNumParam;
+      break;
   }
-  return `The result is: ${output}`
+  return `=> The result is: ${output}`;
 }
 
-console.log('Welcome to Calculator!');
+console.log('=> Welcome to Calculator!');
 
 let number1 = getUserInput("What's the first number?\n");
+
+while (impermissibleNumber(number1)) {
+  console.log("=> Hmm... that doesn't look like a number.");
+  number1 = READLINE.question();
+}
+
 let number2 = getUserInput("What's the second number?\n");
+
+while (impermissibleNumber(number2)) {
+  console.log("=> Hmm... that doesn't look like a number.");
+  number2 = READLINE.question();
+}
+
 number1 = Number(number1);
 number2 = Number(number2);
+
 let operation = getUserInput(
   "What operation would you like to perform?\n1) Add\n2) Subtract\n3) Multiply\n4) Divide\nEnter the number of your choice.\n"
-)
+);
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  console.log("=> Must choose 1, 2, 3 or 4");
+  operation = READLINE.question();
+}
+
 let result = calculator(number1, number2, operation);
 console.log(result);
---
+++ small_programs/examples/debugging-example-1.js
@@ -0,0 +1,26 @@
+function car(newCar) {
+  let make = getMake(newCar);
+  let model = getModel(newCar);
+  return [make, model];
+}
+
+function getMake(newCar) {
+  return newCar.split(' ')[0];
+}
+
+function getModel(newCar) {
+  return newCar.split(' ')[2];
+}
+
+let [ make, model ] = car('Ford Mustang');
+console.log(make === 'Ford');   // => true
+console.log(model[0] === 'M');  // => TypeError: Cannot read property '0' of undefined
+
+[make, model] = car('Mitsubishi Mirage');
+console.log(model[0] === 'M');  // => TypeError: Cannot read property '0' of undefined
+
+[make, model] = car('Chevy Malibu');
+console.log(model[0] === 'M');  // => TypeError: Cannot read property '0' of undefined
+
+[make, model] = car('Toyota Corolla');
+console.log(model[0] === 'M');  // => TypeError: Cannot read property '0' of undefined
