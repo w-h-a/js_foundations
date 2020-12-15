2a9a89bd8d8733107fea11d3ce7c387f0c85928c
Wes Anderson <wesley.herb.anderson@gmail.com>
2020-12-15 16:18:01 -0500

    adding final practice problems

---
+++ practice_problems/hard_1/question-1.js
@@ -0,0 +1,15 @@
+function first() {
+  return {
+    prop1: "hi there"
+  };
+}
+
+function second() {
+  return
+  {
+    prop1: "hi there"
+  };
+}
+
+console.log(first()); // Will this log
+console.log(second()); // the same as this?
---
+++ practice_problems/hard_1/question-3.js
@@ -0,0 +1,50 @@
+/*
+function messWithVars(one, two, three) {
+  one = two;
+  two = three;
+  three = one;
+}
+
+let one = ["one"];
+let two = ["two"];
+let three = ["three"];
+
+messWithVars(one, two, three);
+
+console.log(`one is: ${one}`); // no changes
+console.log(`two is: ${two}`);
+console.log(`three is: ${three}`);
+*/
+/*
+function messWithVars(one, two, three) {
+  one = ["two"];
+  two = ["three"];
+  three = ["one"];
+}
+
+let one = ["one"];
+let two = ["two"];
+let three = ["three"];
+
+messWithVars(one, two, three);
+
+console.log(`one is: ${one}`); // no changes
+console.log(`two is: ${two}`);
+console.log(`three is: ${three}`);
+*/
+
+function messWithVars(one, two, three) {
+  one.splice(0, 1, "two");
+  two.splice(0, 1, "three");
+  three.splice(0, 1, "one");
+}
+
+let one = ["one"];
+let two = ["two"];
+let three = ["three"];
+
+messWithVars(one, two, three);
+
+console.log(`one is: ${one}`); // changes (splice is not slice!)
+console.log(`two is: ${two}`);
+console.log(`three is: ${three}`);
