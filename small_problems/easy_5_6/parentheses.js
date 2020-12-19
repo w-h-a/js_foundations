0a005041a2fb8bd9882d4aad2fb53c8be5847707
Wes Anderson <wesley.herb.anderson@gmail.com>
2020-12-18 19:52:39 -0500

    revising parentheses

--- small_problems/easy_5_6/parentheses.js
+++ small_problems/easy_5_6/parentheses.js
@@ -1,38 +1,48 @@
 /*
+I did not think of this one as "easy" at all. Pseudocode 15-16 or
+JS code 34-35 is crucial, and I didn't come up w/ it myself. I realized
+something like that needed to be added only after peeking at solutions.
+
 START
 
-1.  SET balance variable to 0
-2.  SET arrayRep to split string
-3.  SET reduced variable to true
-4.  WHILE there are elements in arrayRep
+1.  SET arrayRep to split string
+2.  SET reduced variable to object with two arrays (arrayOOpens, arrayOClosed)
+3.  WHILE there are elements in arrayRep
       IF the element is strictly identical to '('
-        SET balance to balance plus 1
+        SET arrayOOpens to include it
       ELSE IF element is strictly identical to ')'
-        SET balance to balance minus 1
-      IF balance is less than 0
-        SET reduced to false
-        RETURN reduced
+        SET arrayOClosed to include it
+      IF length of arrayOClosed > arrayOOpens
+        SET reduced variable to include 'closingBeforeOpening' : true
       RETURN reduced
-5.   RETURN reduced and whether balance is strictly identical to 0
+4.  IF reduced variable's 'closingBeforeOpening' is true
+      RETURN false
+    ELSE
+      RETURN whether arrayOClosed length is strictly identical to arrayOOpens length
 
 END
 */
 
 function isBalanced(stringP) {
-  let balance = 0;
-  let reduced = stringP.split('').reduce((acc, ele) => {
+  let reducedToTwoArrays = stringP.split('').reduce((acc, ele) => {
     if (ele === '(') {
-      balance += 1;
+      acc['arrayOOpens'].push(ele);
     } else if (ele === ')') {
-      balance -= 1;
+      acc['arrayOClosed'].push(ele);
     }
-    if (balance < 0) {
-      acc = false;
-      return acc;
+
+    if (acc['arrayOOpens']['length'] < acc['arrayOClosed']['length']) {
+      acc['closingBeforeOpening'] = true;
     }
+
     return acc;
-  }, true);
-  return reduced && balance === 0;
+  }, {arrayOOpens: [], arrayOClosed: []});
+
+  if (reducedToTwoArrays['closingBeforeOpening']) {
+    return false;
+  } else {
+    return reducedToTwoArrays['arrayOOpens']['length'] === reducedToTwoArrays['arrayOClosed']['length'];
+  }
 }
 
 console.log(isBalanced("What (is) this?") === true);
