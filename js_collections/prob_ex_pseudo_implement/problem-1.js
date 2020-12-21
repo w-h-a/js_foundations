005e96fbe971ec6b31a11a65455bd0566b16e470
Wes Anderson <wesley.herb.anderson@gmail.com>
2020-12-21 17:57:25 -0500

    adding PEPI method practice

---
+++ js_collections/prob_ex_pseudo_implement/problem-1.js
@@ -0,0 +1,6 @@
+
+
+changeMe("We will meet at noon") === "We will meet at NOON";
+changeMe("No palindromes here") === "No palindromes here";
+changeMe("") === "";
+changeMe("I LOVE my mom and dad equally") === "I LOVE my MOM and DAD equally";
---
+++ js_collections/prob_ex_pseudo_implement/problem-2.js
@@ -0,0 +1,12 @@
+
+console.log(palindromeSubstrings("supercalifragilisticexpialidocious"));
+// should return: ["ili"]
+
+console.log(palindromeSubstrings("abcddcbA"));
+// should return: ["bcddcb", "cddc", "dd"]
+
+console.log(palindromeSubstrings("palindrome"));
+// should log: []
+
+console.log(palindromeSubstrings("") === []);
+// should log: true
