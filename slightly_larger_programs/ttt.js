5c6497ef81dfd209358057a5c600502233ac1e4c
Wes Anderson <wesley.herb.anderson@gmail.com>
2021-01-13 09:54:05 -0500

    modifying ttt files

--- slightly_larger_programs/ttt.js
+++ slightly_larger_programs/ttt.js
@@ -1,28 +1,14 @@
-/*
-1. DO
-  - PRINT the intial empty 3x3 board
-  - WHILE there is no winner and the board is not full
-    - GET user's move
-    - GET computer's move
-    - PRINT updated board state
-  - IF the board is a winning board
-    - PRINT winner
-    ELSE
-    - PRINT tie
-  - GET user's response to "Play again?"
-2. WHILE yes
-3. PRINT "Goodbye!"
-*/
+// global variables
+
+let readline = require('readline-sync');
+
+let playAgain;
 
 let unusedSquare = ' ';
 
 let humanMarker = 'X';
 
 let computerMarker = 'O';
-
-let playAgain;
-
-let readline = require('readline-sync');
 
 let playerMoves = {
   1: unusedSquare, // top left
@@ -36,14 +22,54 @@ let playerMoves = {
   9: unusedSquare, // bottom right
 };
 
-function getPlayersChoice(userInputP) {
-  return readline.question(userInputP).trim();
+let winningLines = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+  [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+  [1, 5, 9], [3, 5, 7]             // diagonals
+];
+
+// functions w/o side-effects
+
+function getRandomIdxFromInterval(minP, maxP) {
+  return Math.floor((Math.random() * (maxP - minP + 1)) + minP);
 }
 
 function getEmpties() {
   return Object.keys(playerMoves).filter(ele => {
     return playerMoves[ele] === unusedSquare;
   });
+}
+
+function getIsBoardFull() {
+  return getEmpties()['length'] === 0;
+}
+
+function getWinner() {
+  for (let idx = 0; idx < winningLines['length']; idx += 1) {
+    let [sq1, sq2, sq3] = winningLines[idx];
+
+    if (
+      playerMoves[sq1] === humanMarker &&
+      playerMoves[sq2] === humanMarker &&
+      playerMoves[sq3] === humanMarker
+    ) {
+      return 'You';
+    } else if (
+      playerMoves[sq1] === computerMarker &&
+      playerMoves[sq2] === computerMarker &&
+      playerMoves[sq3] === computerMarker
+    ) {
+      return 'Computer';
+    }
+  }
+
+  return null;
+}
+
+// functions w/ side-effects
+
+function getPlayerInput(userInputP) {
+  return readline.question(userInputP).trim().toLowerCase();
 }
 
 function initializeBoard() {
@@ -53,6 +79,10 @@ function initializeBoard() {
 }
 
 function displayBoard() {
+  console.clear();
+
+  console.log(`You are ${humanMarker}. Computer is ${computerMarker}.`);
+
   console.log('');
   console.log('     |     |');
   console.log(`  ${playerMoves['1']}  |  ${playerMoves['2']}  |  ${playerMoves['3']}`);
@@ -68,29 +98,72 @@ function displayBoard() {
   console.log('');
 }
 
+function humanMoves() {
+  let emptySquares = getEmpties();
+  let humanSquare = getPlayerInput(`Choose a square (${emptySquares.join(', ')}):\n`);
+  while (!emptySquares.includes(humanSquare)) {
+    console.log("That is not an empty square.");
+    humanSquare = getPlayerInput();
+  }
+  playerMoves[humanSquare] = humanMarker;
+}
+
+function computerMoves() {
+  let emptySquares = getEmpties();
+  let draw = getRandomIdxFromInterval(0, emptySquares['length'] - 1);
+  let compSquare = emptySquares[draw];
+  playerMoves[compSquare] = computerMarker;
+}
+
+// pseudocode
+
+/*
+1. DO
+  - SET 3x3 board to empty
+  - WHILE there is no winner and the board is not full
+    - PRINT updated board
+    - GET user's move
+    - IF there is a winner or the board is full
+      - BREAK out of WHILE
+    - GET computer's move
+  - PRINT updated board
+  - IF the board is a winning board
+    - PRINT winner
+    ELSE
+    - PRINT tie
+  - GET user's response to "Play again?"
+2. WHILE yes
+  - GO back to step 1
+3. PRINT "Goodbye!"
+*/
+
+// program
+
 do {
-  console.clear();
   initializeBoard();
+
+  while (!getWinner() && !getIsBoardFull()) {
+    displayBoard();
+
+    humanMoves();
+
+    if (getWinner() || getIsBoardFull()) break;
+
+    computerMoves();
+  }
+
   displayBoard();
 
-  // while (false) {
-    let emptySquares = getEmpties();
-    let humanSquare = getPlayersChoice(`Choose a square (${emptySquares.join(', ')}):\n`);
-    while (!emptySquares.includes(humanSquare)) {
-      console.log("That is not an empty square.");
-      humanSquare = getPlayersChoice();
-    }
-    playerMoves[humanSquare] = humanMarker;
 
+  if (getWinner()) {
+    console.log(`${getWinner()} won!`);
+  } else {
+    console.log("The game is a draw!");
+  }
 
-    emptySquares = getEmpties();
-    let draw = Math.floor(Math.random() * emptySquares['length']);
-    let compSquare = emptySquares[draw];
-    playerMoves[compSquare] = computerMarker;
-    
-    displayBoard();
-  // }
-
-  playAgain = false;
+  playAgain = getPlayerInput(
+    "Enter 'y' if you'd like another go; otherwise enter any key or press enter to exit.\n"
+  ) === 'y';
 } while (playAgain);
+
 console.log("Goodbye!");
--- slightly_larger_programs/ttt.md
+++ slightly_larger_programs/ttt.md
@@ -2,18 +2,22 @@ Problem:
 
 Tic Tac Toe is a 2-player game played on a 3x3 grid called the board. Each player takes a turn and marks a square on the board. The first player to get 3 squares in a row---horizontally, vertically, or diagonally---wins. The game is a tie just in case all 9 squares are filled and neither player has 3 in a row.
 
-Pseudocode (second pass):
+Pseudocode (third pass):
 
 1. DO
-  - PRINT the intial empty 3x3 board
+  - SET 3x3 board to empty
   - WHILE there is no winner and the board is not full
+    - PRINT updated board
     - GET user's move
+    - IF there is a winner or the board is full
+      - BREAK out of WHILE
     - GET computer's move
-    - PRINT updated board state
+  - PRINT updated board
   - IF the board is a winning board
     - PRINT winner
     ELSE
     - PRINT tie
   - GET user's response to "Play again?"
 2. WHILE yes
+  - GO back to step 1
 3. PRINT "Goodbye!"
