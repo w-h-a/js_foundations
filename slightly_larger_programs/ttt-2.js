d29a290516e856ce79358f80bcf1aed5d517ef8e
Wes Anderson <wesley.herb.anderson@gmail.com>
2021-01-16 18:42:39 -0500

    modifying ttt

--- slightly_larger_programs/ttt-2.js
+++ slightly_larger_programs/ttt-2.js
@@ -4,8 +4,6 @@ let readline = require('readline-sync');
 
 let playAgain;
 
-let seeCompReasoning = false;
-
 let doWelcome = true;
 
 let unusedSquare = ' ';
@@ -32,28 +30,16 @@ let winningLines = [
   [1, 5, 9], [3, 5, 7]             // diagonals
 ];
 
-let arrayOfPossibleGames = [];
-
 let cache = {};
 
 let entry = '0';
 
 // functions w/o side-effects
 
-/* no longer needed
-function getRandomIdxFromInterval(minParam, maxParam) {
-  return Math.floor((Math.random() * (maxParam - minParam + 1)) + minParam);
-}
-*/
-
 function getEmpties(movesParam = playerMoves) {
   return Object.keys(movesParam).filter(ele => {
     return movesParam[ele] === unusedSquare;
   });
-}
-
-function getIsBoardFull(movesParam = playerMoves) {
-  return getEmpties(movesParam)['length'] === 0;
 }
 
 function getWinner(movesParam = playerMoves) {
@@ -83,15 +69,6 @@ function joinWith(arrParam, delimParam = ', ', wordParam = 'or') {
   if (arrParam['length'] === 2) return `${arrParam[0]} ${wordParam} ${arrParam[1]}`;
   let initialStr = arrParam.slice(0, arrParam['length'] - 1).join(delimParam);
   return `${initialStr}${delimParam}${wordParam} ${arrParam[arrParam['length'] - 1]}`;
-}
-
-function getStepsForward(movesParam) {
-  return arrayOfPossibleGames.reduce((acc, ele, idx) => {
-    if (movesParam === ele) {
-      acc = idx;
-    }
-    return acc;
-  }, undefined);
 }
 
 function minHumanMaxComputer(playerParam, possParam) {
@@ -163,7 +140,7 @@ function initializeBoard() {
 }
 
 function displayBoard(movesParam = playerMoves) {
-  if (!seeCompReasoning) console.clear();
+  console.clear();
 
   console.log(`You are ${humanMarker}. Computer is ${computerMarker}.`);
 
@@ -182,11 +159,6 @@ function displayBoard(movesParam = playerMoves) {
   console.log('');
 }
 
-function chooseSquare(playerParam) {
-  if (playerParam === 'human') humanMoves();
-  if (playerParam === 'computer') computerMoves();
-}
-
 function humanMoves() {
   let emptySquares = getEmpties();
   let humanSquare = getHumanInput(`Choose a square (${joinWith(emptySquares)}):\n`);
@@ -196,15 +168,6 @@ function humanMoves() {
   }
   playerMoves[humanSquare] = humanMarker;
 }
-
-/* Old computerMoves
-function computerMoves() {
-  let emptySquares = getEmpties();
-  let draw = getRandomIdxFromInterval(0, emptySquares['length'] - 1);
-  let compSquare = emptySquares[draw];
-  playerMoves[compSquare] = computerMarker;
-}
-*/
 
 function computerMoves() {
   if (checkInMemory()) {
@@ -217,30 +180,17 @@ function computerMoves() {
 }
 
 function getStrategy(movesParam, playerParam) {
-  if (movesParam === playerMoves) arrayOfPossibleGames = [];
-
   let copyOfMoves = Object.assign({}, movesParam);
-  arrayOfPossibleGames.push(copyOfMoves);
-
-  if (getWinner(copyOfMoves) === 'You') return {score: -1};
-  if (getWinner(copyOfMoves) === "Computer") return {score: 1};
-  if (getIsBoardFull(copyOfMoves)) return {score: 0};
 
   let emptySquares = getEmpties(copyOfMoves);
 
+  if (getWinner(copyOfMoves) === 'You') return {score: -1};
+  if (getWinner(copyOfMoves) === "Computer") return {score: 1};
+  if (emptySquares['length'] === 0) return {score: 0};
+
   let possibleMoves = getMoves(emptySquares, copyOfMoves, playerParam);
 
-  let stepsForward = getStepsForward(copyOfMoves);
-
-  if (seeCompReasoning) {
-    displayCompSum(copyOfMoves, stepsForward, playerParam, possibleMoves);
-  }
-
   let optimalMove = minHumanMaxComputer(playerParam, possibleMoves);
-
-  if (seeCompReasoning) {
-    displayCompOptim(playerParam, optimalMove, possibleMoves);
-  }
 
   if (playerParam === 'computer') {
     let chosenSquare = possibleMoves[optimalMove];
@@ -250,11 +200,6 @@ function getStrategy(movesParam, playerParam) {
   return possibleMoves[optimalMove];
 }
 
-function addToMemory(movesParam, squareParam) {
-  entry = String(Number(entry) + 1);
-  cache[entry] = {gameState: movesParam, bestMove: squareParam};
-}
-
 function getMoves(emptiesParam, movesParam, playerParam) {
   return emptiesParam.reduce((acc, ele) => {
     let move = {};
@@ -262,12 +207,11 @@ function getMoves(emptiesParam, movesParam, playerParam) {
     move['square'] = ele;
 
     if (playerParam === 'human') {
-      getHumanMove(movesParam, playerParam, ele, move);
+      getHumanMove(movesParam, ele, move);
     } else {
-      getComputerMove(movesParam, playerParam, ele, move);
+      getComputerMove(movesParam, ele, move);
     }
 
-    arrayOfPossibleGames.pop();
     movesParam[ele] = temp;
 
     acc.push(move);
@@ -275,73 +219,19 @@ function getMoves(emptiesParam, movesParam, playerParam) {
   }, []);
 }
 
-function getHumanMove(movesParam, playerParam, eleParam, singleMoveParam) {
+function getHumanMove(movesParam, eleParam, singleMoveParam) {
   movesParam[eleParam] = humanMarker;
-
-  let stepsForward = getStepsForward(movesParam);
-
-  if (seeCompReasoning) {
-    displayCompPrem(playerParam, eleParam, stepsForward, movesParam);
-  }
-
   singleMoveParam['score'] = getStrategy(movesParam, "computer")['score'];
-
-  if (seeCompReasoning) {
-    displayCompConcl(playerParam, singleMoveParam, stepsForward);
-  }
 }
 
-function getComputerMove(movesParam, playerParam, eleParam, singleMoveParam) {
+function getComputerMove(movesParam, eleParam, singleMoveParam) {
   movesParam[eleParam] = computerMarker;
-
-  let stepsForward = getStepsForward(movesParam);
-
-  if (seeCompReasoning) {
-    displayCompPrem(playerParam, eleParam, stepsForward, movesParam);
-  }
-
   singleMoveParam['score'] = getStrategy(movesParam, "human")['score'];
-
-  if (seeCompReasoning) {
-    displayCompConcl(playerParam, singleMoveParam, stepsForward);
-  }
 }
 
-function displayCompPrem(playerParam, eleParam, stepsParam, movesParam) {
-  if (getEmpties(playerMoves)['length'] < 4) {
-    console.log(`The computer is ready to think about what would happen if ${playerParam} chose ${Number(eleParam)}, ${stepsParam + 1} step(s) forward.`);
-    displayBoard(movesParam);
-    getHumanInput("When you are ready, enter any key or hit enter to continue.\n");
-  }
-}
-
-function displayCompConcl(playerParam, singleMoveParam, stepsParam) {
-  if (getEmpties(playerMoves)['length'] < 4 && singleMoveParam['score'] === -1) {
-    console.log(`The computer sees you'd win and assigns a -1 to the outcome of this series of moves,\n which is ${stepsParam + 1} step(s) forward and ends with ${playerParam}'s ${singleMoveParam['square']}.`);
-    getHumanInput("When you are ready, enter any key or hit enter to continue.\n");
-  } else if (getEmpties(playerMoves)['length'] < 4 && singleMoveParam['score'] === 0) {
-    console.log(`The computer sees the game would end in a draw and assigns a 0 to the outcome of this series of moves,\n which is ${stepsParam + 1} step(s) forward and ends with ${playerParam}'s ${singleMoveParam['square']}.`);
-    getHumanInput("When you are ready, enter any key or hit enter to continue.\n");
-  } else if (getEmpties(playerMoves)['length'] < 4 && singleMoveParam['score'] === 1) {
-    console.log(`The computer sees it'd win and assigns a +1 to the outcome of this series of moves,\n which is ${stepsParam + 1} step(s) forward and ends with ${playerParam}'s ${singleMoveParam['square']}.`);
-    getHumanInput("When you are ready, enter any key or hit enter to continue.\n");
-  }
-}
-
-function displayCompSum(movesParam, stepsParam, playerParam, possParam) {
-  if (getEmpties(playerMoves)['length'] < 4) {
-    console.log("From the starting assumption of...");
-    displayBoard(movesParam);
-    console.log(`...which is ${stepsParam} step(s) forward,\nthe computer consdered the following ${playerParam} moves:`, possParam);
-    getHumanInput("When you are ready, enter any key or hit enter to continue.\n");
-  }
-}
-
-function displayCompOptim(playerParam, optimParam, possParam) {
-  if (getEmpties(playerMoves)['length'] < 4) {
-    console.log(`${playerParam}'s optimal move is:`, possParam[optimParam]);
-    getHumanInput("When you are ready, enter any key or hit enter to continue.\n");
-  }
+function addToMemory(movesParam, squareParam) {
+  entry = String(Number(entry) + 1);
+  cache[entry] = {gameState: movesParam, bestMove: squareParam};
 }
 
 // program
@@ -351,17 +241,19 @@ do {
     console.clear();
     console.log("Welcome to Tic Tac Toe!");
     doWelcome = false;
-    getHumanInput("When you are ready, enter any key or hit enter to continue.\n");
   }
 
   initializeBoard();
 
-  let currentPlayer = 'human';
+  let currentPlayer = (
+    getHumanInput("Enter 'c' if you'd like the computer to go first; otherwise, enter any key or hit enter.\n") === 'c' ? 'computer' : 'human'
+  );
 
-  while (!getWinner() && !getIsBoardFull()) {
+  while (!getWinner() &&  getEmpties()['length'] !== 0) {
     displayBoard();
 
-    chooseSquare(currentPlayer);
+    if (currentPlayer === 'human') humanMoves();
+    if (currentPlayer === 'computer') computerMoves();
 
     currentPlayer = (currentPlayer === 'human' ? 'computer' : 'human');
   }
--- slightly_larger_programs/ttt.md
+++ slightly_larger_programs/ttt.md
@@ -45,64 +45,124 @@ Pseudocode:
 
 IF the input array is length < 2
   - RETURN joined array
-  ELSE IF the input array is length === 2
+ELSE IF the input array is length === 2
   - RETURN "num1 ${word} num2"
-  ELSE
+ELSE
   - RETURN joined array except for last item + delimiter + word + last item
 
 Bonus Problem 2:
 
-Create an improved AI.
+Allow computer or human to go first.
 
-for getStrategy(playerMoves, currentPlayer)
-1. SET copyOfMoves set copy of possible playersMove object
-2. SET emptySquares to getEmpties(copyOfMoves)
-3. IF human is the winner
-  - RETURN {score: -1}
+Bonus Problem 3:
+
+Improve AI
+
+for computerMoves()
+IF GET checkInMemory() RETURNS true
+  - SET compSquare to value at 'square' key of object from GET pullFromMemory()
+  - SET compSquare of playerMoves to computerMarker
+ELSE
+  - SET compSquare to value at 'square' key of object from GET getStrategy(playerMoves, "computer")
+  - SET compSquare of playerMoves to computerMarker
+
+for getStrategy(movesParam, playerParam)
+1. SET copyOfMoves to copy of playerMoves object
+2. SET emptySquares to RETURN value of getEmpties(copyOfMoves)
+3. IF user is the winner
+  - RETURN object with 'score', -1 key-value pair
   ELSE IF computer is the winner
-  - RETURN {score: 1}
-  ELSE IF board full
-  - RETURN {score: 0}
-4. SET possibleMoves to getMoves(emptySquares, copyOfMoves, playerParam)
-5. SET optimalMove to undefined
-6. IF player is human
-  - SET optimalMove to getHumanBestMove(moves)
-  ELSE
-  - SET optimalMove to getComputerBestMove(moves)
-7. RETURN possibleMoves[optimalMove]
+  - RETURN object with 'score', +1 key-value pair
+  ELSE IF emptySquares length is 0
+  - RETURN object with 'score', 0 key-value pair
+4. SET possibleMoves to RETURN value of getMoves(emptySquares, copyOfMoves, playerParam)
+5. SET optimalMove to RETURN value of minHumanMaxComputer(playerParam, possibleMoves)
+6. IF playerParam is 'computer'
+  - SET chosenSquare to element of possibleMoves at index optimalMove
+  - GET addToMemory(copyOfMoves, chosenSquare)
+7. RETURN element of possibleMoves at index optimalMove
 
-for getMoves(empties, playerMoves, playerParam)
-1. SET possMoves to array of length 0
-2. WHILE there are elements in empties
-  - SET move be an object
-  - SET temp variable to the value of playerMoves given key of index empties
-  - SET move['key'] to element at idx of empties
-  - IF playerMoves is human
-    - SET playerMoves[emptiesParam[idx]] to humanMarker
-    - SET result to getStrategy(playerMoves, currentPlayer)
-    - SET move['score'] to result['score']
+for getMoves(emptiesParam, movesParam, playerParam)
+1. SET possibleMoves to array of length 0
+2. WHILE emptiesParam has elements
+  - SET move to {}
+  - SET temp variable to value of movesParam at element
+  - SET value of move's 'square' key to element
+  - IF playerParm is 'human'
+    - GET getHumanMove(movesParam, ele, move)
     ELSE
-    - SET playerMoves[emptiesParam[idx]] to computerMarker
-    - SET result to getStrategy(playerMoves, currentPlayer)
-    - SET move['score'] to result['score']
-  - SET playerMoves[emptiesParam[idx]] to temp
-  - SET possMoves to inlucde move
-3. RETURN possMoves
+    - GET getComputerMove(movesParam, ele, move)
+  - SET movesParam at element back to temp variable's value
+  - SET possibleMoves to include move object
+3. RETURN possibleMoves
 
-for getHumanBestMove(possibleMoves)
-1. SET result to undefined
-2. SET minScore to Infinity
-3. WHILE there are elements in possibleMoves
-  - IF ele['score'] less than minScore
-    - SET minScore to ele['score']
-    - SET reuslt to idx
-4. RETURN result
+for getHumanMove(movesParam, eleParam, singleMoveParam)
+1. SET value of movesParam at eleParam to humanMarker
+2. SET value of singleMoveParam at 'score' to RETURN value at 'score' key of getStrategy(movesParam, "computer")
 
-for getComputerBestMove(possibleMoves)
-1. SET result to undefined
-2. SET maxScore to -Infinity
-3. WHILE there are elements in possibleMoves
-  - IF ele['score'] greater than maxScore
-    - SET maxScore to ele['score']
-    - SET reuslt to idx
-4. RETURN result
+for getComputerMove(movesParam, eleParam, singleMoveParam)
+1. SET value of movesParam at eleParam to computerMarker
+2. SET value of singleMoveParam at 'score' to RETURN value at 'score' key of getStrategy(movesParam, "human")
+
+for minHumanMaxComputer(playerParam, possParam)
+IF playerParam is 'human'
+  - RETURN getHumanBestMove(possParam)
+ELSE
+  - RETURN getComputerBestMove(possParam)
+
+for getHumanBestMove(possParam)
+1. SET minSocre to Infinity
+2. SET indexOfBestMove to undefined
+3. WHILE possParam has elements
+  - IF value at 'score' key of ele (move) is less than minScore
+    - SET minScore to value at 'score' key of ele
+    - SET indexOfBestMove to idx of current element
+4. RETURN indexOfBestMove
+
+for getComputerBestMove(possParam)
+1. SET maxSocre to -Infinity
+2. SET indexOfBestMove to undefined
+3. WHILE possParam has elements
+  - IF value at 'score' key of ele (move) is greater than maxScore
+    - SET maxScore to value at 'score' key of ele
+    - SET indexOfBestMove to idx of current element
+4. RETURN indexOfBestMove
+
+for addToMemory(movesParam, squareParam)
+1. SET entry to numberified version of itself
+2. SET entry to entry plus 1
+3. SET entry to stringified version of itself
+4. SET entry value at entry key of cache to
+  - gameState: movesParam
+  - bestMove: squareParam
+
+for checkInMemory()
+1. SET currGameState to entries of playerMoves
+2. SET result1 to false
+3. SET arr to entries of cache
+4. WHILE there are elements in arr
+  - SET obj to element at index 1
+  - SET src to entries of value at 'gameState' key of obj
+  - SET result2 to true
+  - WHILE there are elements in src
+    - IF src and currentGameState's value don't match up at each idx
+      - SET result2 to false
+  - SET result1 to result2
+5. RETURN result1
+
+for pullFromMemory()
+1. SET currGameState to entries of playerMoves
+2. SET result1 to empty array
+3. SET arr to entries of cache
+4. WHILE there are elements in arr
+  - SET obj to element at index 1
+  - SET src to entries of value at 'gameState' key of obj
+  - SET result2 to true
+  - WHILE there are elements in src
+    - IF src and currentGameState's value don't match up at each idx
+      - SET result2 to false
+  - SET result1 to include the unique element of arr for which result2 remains true
+5. SET result1 to its element at index 0 (the unique element of arr); this has two elements, the 'entry' key and the object value with gameState and bestMove properties.
+6. SET result1 to its element at index 1
+7. SET result1 to bestMove property, which is itself an object with 'square' and 'score' properties
+8. RETURN result1
